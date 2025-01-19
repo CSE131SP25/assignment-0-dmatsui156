a832a8377c0b86b61f9cd27915b6c172c670aaf7
dmatsui24bregencatholichs.org <dmatsui24bregencatholichs.org@s24dmatsui.dhcp.wustl.edu>
2025-01-18 19:57:12 -0600

    Finished up to Messege

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -34,6 +34,16 @@ public class RobotInstructions  {
 		robot.forward(23);
 		robot.turnRight(90);
 		robot.forward(80);
+		robot.turnRight(90);
+		robot.forward(50);
+		robot.turnRight(90);
+		robot.forward(80);
+		robot.turnRight(90);
+		robot.forward(23);
+		robot.turnLeft(90);
+		robot.forward(60);
+		robot.turnRight(90);
+		robot.forward(6);
 	}
 
 }
